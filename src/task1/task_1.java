bf7f3f96d0732aa7597082e7ce13e2d611425f5f
yuzihao <yzh@example.com>
2021-04-08 21:02:21 +0800

    更新

--- src/task1/task_1.java
+++ src/task1/task_1.java
@@ -1,12 +1,15 @@
 package task1;
 
-
+import java.util.Scanner;
 
 public class task_1 {
 	public static void main(String[] args) {
-		int[] array = {1, 6, 10, 11, 12, 100};
+		int[] array = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+		System.out.println("请输入1-15的值");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
 		for (int i = 0; i < array.length; i++) {
-			if(array[i]==12) {
+			if(array[i]==num) {
 				System.out.println("下标是"+i);
 			}
 		}
